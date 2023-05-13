553341f23a8717477b2d4cefb30c99867f7d0292
skuttenkuler <sam.kuttenk@gmail.com>
2023-05-13 12:45:23 -0500

    added location page, started booking contact form

--- src/pages/Booking/Booking.tsx
+++ src/pages/Booking/Booking.tsx
@@ -1,11 +1,61 @@
-import React from "react"
 
-export interface IBookingPageProps {};
+// import { render } from '@react-email/render';
+// import AWS from 'aws-sdk';
+import * as React from 'react';
+// import { Email } from '../../components/Email/Email';
 
-const BookingPage: React.FunctionComponent<IBookingPageProps> = (props) => {
-    return (
-        <div>booking</div>
-    )
-};
+// AWS.config.update({ region: process.env.AWS_SES_REGION });
+// const emailHtml = render(<Email url="https://wealthandtastetattoo.com" />);
 
+// const options = {
+//     Source: process.env.FROM_EMAIL,
+//     Destination: {
+//       ToAddresses: [process.env.TO_EMAIL],
+//     },
+//     Message: {
+//       Body: {
+//         Html: {
+//           Charset: 'UTF-8',
+//           Data: emailHtml,
+//         },
+//       },
+//       Subject: {
+//         Charset: 'UTF-8',
+//         Data: 'NEW PATIENT INQUIRY',
+//       },
+//     },
+//   };
+//   const sendPromise = new AWS.SES({ apiVersion: '2010-12-01' })
+//   .sendEmail(options)
+//   .promise();
+ const BookingPage: React.FunctionComponent = () => {
+return (
+//         <section className=" booking-sec">
+//             <h4 id="form-h">New Client Form</h4>
+//             <div className="booking-container">
+//             <section className="booking-left">
+//                 <div className="text-container">
+//                     <p>Please include the following and we'll match you with the correct artist:</p>
+//                     <ol>
+//                         <li>Location</li>
+//                         <li>Size</li>
+//                         <li>A brief description</li>
+//                         <li>Style</li>
+//                         <li>Color? Black and Grey?</li>
+//                         <li>A reference photo.</li>
+//                     </ol>
+//                 </div>
+//             </section>
+//             <section className="booking-right">
+               
+//                     <form action="" method="POST" className="contact-form" enctype="multipart/form-data">
+//                         <div className="form-control">
+//                             <input type="submit" value="Send"/>
+//                         </div>
+//                     </form>
+//             </section>
+//         </div>
+//     </section>
+//     
+<></>)};
 export default BookingPage;
--- src/pages/Location/Location.tsx
+++ src/pages/Location/Location.tsx
@@ -1,10 +1,22 @@
 import React from "react"
 
-export interface ILocationPageProps {};
 
-const LocationPage: React.FunctionComponent<ILocationPageProps> = (props) => {
+const LocationPage: React.FunctionComponent = () => {
     return (
-        <div>Locaiton</div>
+        <div className="page-l">
+        <section className="location-sec">
+        <div className="container">
+            <h4 className="location-header">Wealth and Taste Tattoo</h4>
+            <img src="https://wat-asset-bucket.s3.us-east-2.amazonaws.com/assets/assets/shop/location.png" alt="Shop Image"/>
+            <ul>
+            <br></br>
+            <p>3250 Dickerson Pike Suite 121</p>
+            <p>Nashville, TN 37207</p>
+            <p>(615)-660-0811</p>
+            </ul>
+        </div>
+    </section>
+</div>
     )
 };
 
